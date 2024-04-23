e16f8359be8c71a0480b03f8accac5996be4b9f5
Matthew To <matthewto2is@gmail.com>
2024-04-23 23:11:58 +0100

    Start of projectt
    This is the core of the program. In here the bot can be initalised.

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-#Python main file
+# Import the required modules
+import discord
+import os
+from discord.ext import commands 
+from dotenv import load_dotenv
+
+# Create a Discord client instance and set the command prefix
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='cmd ', intents=intents)
+
+# Set the confirmation message when the bot is ready
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+
+# Retrieve token from the .env file
+load_dotenv()
+bot.run(os.getenv('TOKEN'))
